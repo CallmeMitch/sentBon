e4650dae3a23f58e5197adf909f97cd5820a31d8
CallmeMitch <valentin.brison@gmail.com>
2025-04-27 22:57:52 +0200

    feat: add gui interface, add alla printer of NIC, mute listenerFrameFunction

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-	"sentbon/getIP"
+	"sentbon/utils"
+
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/widget"
 )
 
 func main() {
 	//fmt.Println("Starting")
 
-	//utils.LocalAddresses()
+	utils.LocalAddresses()
 
-	getIP.ReadNIC()
+	//getIP.ReadNIC()
+
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+
 }
--- utils/utils.go
+++ utils/utils.go
@@ -6,32 +6,20 @@ import (
 )
 
 func LocalAddresses() {
+
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		fmt.Printf("localAddresses: %+v\n", err.Error())
 		return
 	}
-
-	ipv4, err := net.InterfaceAddrs()
-	if err != nil {
-		fmt.Println(err)
+	choice := 19
+	if choice >= len(ifaces) {
+		fmt.Printf("%d\n", choice)
 	}
 
 	for _, iface := range ifaces {
-		net := iface.Name
-		if net == "wlp0s20f3" {
-			fmt.Println(net)
-		} else {
-			continue
-		}
+		fmt.Printf("Nom d'interface disponible sur la machine: %s\n", iface.Name)
 
-		for _, IP := range ipv4 {
-			//for _, parsedIP := range net.ParseCIDR(IP) {
-			//	fmt.Println(parsedIP)
-			//}
-
-			//ipParsed := net.ParseCIDR(IP)
-			fmt.Println(IP)
-		}
 	}
+
 }
